fd89b10536ff80b531a8b8e49d84264b6b2e74bf
Roberto Rodriguez <robertrreyes86@gmail.com>
2018-09-28 12:08:26 -0400

    added the css usin bootstrap, pass validation using regex

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, redirect, render_template
 import cgi
+import re
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-@app.route("/welcome", methods=['GET'])
+
+@app.route('/welcome', methods=['POST'])
 def welcome():
     user = request.form['username']
     return render_template('welcome.html', username = user)
 
-@app.route("/", methods=['POST'])
+@app.route('/', methods=['POST'])
 def validate_password():
 
     space = [' ']
@@ -25,6 +27,9 @@ def validate_password():
     password_error = ''
     verify_error = ''
     email_error = ''
+    
+    
+
 
     if len(username) < 3 or len(username) > 20 or username == "" or any(char in space for char in username):
         username_error = 'That is not a valid username'
@@ -40,6 +45,11 @@ def validate_password():
         password = ''
         verify = ''
         email = email
+    if not re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+        password_error = 'Type 8 char including at least one number, special char, capital and lower letter.'
+        password = '' 
+        username = username
+        email = email    
     if  email != "":
         if len(email) < 3 or len(email) > 20 or not any(char in special for char in email):
             email_error = 'That is not a valid email'
@@ -50,12 +60,12 @@ def validate_password():
 
 
     if not username_error and not password_error and not verify_error and not email_error:
-        return render_template('welcome.html', username = username)
+        return render_template('welcome.html', title = "Welcome", username = username)
     else:
-        return render_template('index.html',username = username, email = email, username_error = username_error, password_error = password_error, verify_error = verify_error, email_error = email_error)
+        return render_template('index.html',title="Signup", username = username, email = email, username_error = username_error, password_error = password_error, verify_error = verify_error, email_error = email_error)
   
-@app.route("/")
+@app.route('/')
 def display_index():
-    return render_template('index.html')
+    return render_template('index.html', title = "Signup")
 
 app.run()
