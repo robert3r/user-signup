6df27922f93f2f2bf6d0559622b3d6f67efbd4ea
Roberto Rodriguez <robertrreyes86@gmail.com>
2018-10-01 12:31:35 -0400

    changed the html document name from index to register

--- main.py
+++ main.py
@@ -27,9 +27,6 @@ def validate_password():
     password_error = ''
     verify_error = ''
     email_error = ''
-    
-    
-
 
     if len(username) < 3 or len(username) > 20 or username == "" or any(char in space for char in username):
         username_error = 'That is not a valid username'
@@ -49,9 +46,7 @@ def validate_password():
         username = username
         password = ''
         verify = ''
-        email = email
-
-    
+        email = email 
           
     """ if  email != "":
         if len(email) < 3 or len(email) > 20 or not any(char in special for char in email) """
@@ -67,10 +62,10 @@ def validate_password():
     if not username_error and not password_error and not verify_error and not email_error:
         return render_template('welcome.html', title = "Welcome", username = username)
     else:
-        return render_template('index.html',title="Signup", username = username, email = email, username_error = username_error, password_error = password_error, verify_error = verify_error, email_error = email_error)
+        return render_template('register.html',title="Signup", username = username, email = email, username_error = username_error, password_error = password_error, verify_error = verify_error, email_error = email_error)
   
 @app.route('/')
 def display_index():
-    return render_template('index.html', title = "Signup")
+    return render_template('register.html', title = "Signup")
 
 app.run()
